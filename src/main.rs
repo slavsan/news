e6299010458069959ce95fe8283b4ea38b273ed6
Slavcho Slavchev <slavsan.mail@gmail.com>
2022-09-05 22:23:54 +0300

    Add lists and switching between blocks

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ use std::{error::Error, io};
 use tui::{
     backend::{Backend, CrosstermBackend},
     style::{Color,Style,Modifier},
-    text::{Span},
-    layout::{Alignment,Constraint, Direction, Layout, Rect},
-    widgets::{Block, Borders,BorderType},
+    text::{Span, Spans},
+    layout::{Alignment,Constraint, Direction, Layout, Rect, Corner},
+    widgets::{Block, Borders, BorderType, ListState, ListItem, List},
     Frame, Terminal,
 };
 
@@ -20,9 +20,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
+    let app = App::new();
 
     // create app and run it
-    let res = run_app(&mut terminal);
+    let res = run_app(&mut terminal, app);
 
     // restore terminal
     disable_raw_mode()?;
@@ -40,19 +41,169 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn run_app<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
+struct StatefulList<T> {
+    state: ListState,
+    items: Vec<T>,
+}
+
+impl<T> StatefulList<T> {
+    fn with_items(items: Vec<T>) -> StatefulList<T> {
+        StatefulList {
+            state: ListState::default(),
+            items,
+        }
+    }
+
+    fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.items.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.items.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    fn unselect(&mut self) {
+        self.state.select(None);
+    }
+}
+
+struct App<'a> {
+    items: StatefulList<(&'a str, usize)>,
+    articles: StatefulList<(&'a str, usize)>,
+    events: Vec<(&'a str, &'a str)>,
+    selected: i8,
+}
+
+impl<'a> App<'a> {
+    fn new() -> App<'a> {
+        App {
+            selected: 0,
+            items: StatefulList::with_items(vec![
+                ("Item0", 1),
+                ("Item1", 2),
+                ("Item2", 1),
+                ("Item3", 3),
+                ("Item4", 1),
+                ("Item5", 4),
+                ("Item6", 1),
+                ("Item7", 3),
+                ("Item8", 1),
+                ("Item9", 6),
+                ("Item10", 1),
+                ("Item11", 3),
+                ("Item12", 1),
+                ("Item13", 2),
+                ("Item14", 1),
+                ("Item15", 1),
+                ("Item16", 4),
+                ("Item17", 1),
+                ("Item18", 5),
+                ("Item19", 4),
+                ("Item20", 1),
+                ("Item21", 2),
+                ("Item22", 1),
+                ("Item23", 3),
+                ("Item24", 1),
+            ]),
+            articles: StatefulList::with_items(vec![
+                ("Article Item0", 1),
+                ("Article Item1", 2),
+                ("Article Item2", 1),
+                ("Article Item3", 3),
+                ("Article Item4", 1),
+                ("Article Item5", 4),
+                ("Article Item6", 1),
+            ]),
+            events: vec![
+                ("Event1", "INFO"),
+                ("Event2", "INFO"),
+                ("Event3", "CRITICAL"),
+                ("Event4", "ERROR"),
+                ("Event5", "INFO"),
+                ("Event6", "INFO"),
+                ("Event7", "WARNING"),
+                ("Event8", "INFO"),
+                ("Event9", "INFO"),
+                ("Event10", "INFO"),
+                ("Event11", "CRITICAL"),
+                ("Event12", "INFO"),
+                ("Event13", "INFO"),
+                ("Event14", "INFO"),
+                ("Event15", "INFO"),
+                ("Event16", "INFO"),
+                ("Event17", "ERROR"),
+                ("Event18", "ERROR"),
+                ("Event19", "INFO"),
+                ("Event20", "INFO"),
+                ("Event21", "WARNING"),
+                ("Event22", "INFO"),
+                ("Event23", "INFO"),
+                ("Event24", "WARNING"),
+                ("Event25", "INFO"),
+                ("Event26", "INFO"),
+            ],
+        }
+    }
+
+    fn nextBlock(&mut self) {
+        if self.selected == 2 {
+            self.selected = 0;
+            return
+        }
+        self.selected += 1;
+    }
+}
+
+fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
     loop {
-        terminal.draw(ui)?;
+        terminal.draw(|f| ui(f, &mut app))?;
 
         if let Event::Key(key) = event::read()? {
-            if let KeyCode::Char('q') = key.code {
-                return Ok(());
+            match key.code {
+                KeyCode::Char('q') => return Ok(()),
+                // KeyCode::Left => app.items.unselect(),
+                KeyCode::Down | KeyCode::Char('j') => {
+                    if app.selected == 0 {
+                        app.items.next()
+                    } else if app.selected == 1 {
+                        app.articles.next()
+                    }
+                },
+                KeyCode::Up | KeyCode::Char('k') => {
+                    if app.selected == 0 {
+                        app.items.previous()
+                    } else if app.selected == 1 {
+                        app.articles.previous()
+                    }
+                },
+                KeyCode::Tab => app.nextBlock(),
+                _ => {}
             }
         }
     }
 }
 
-fn ui<B: Backend>(f: &mut Frame<B>) {
+fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
     // Wrapping block for a group
     // Just draw the block and the group on the same area and build the group
     // with at least a margin of 1
@@ -66,13 +217,20 @@ fn ui<B: Backend>(f: &mut Frame<B>) {
     ].as_ref())
     .split(f.size());
 
+    let foo = chunks[0];
+
     // Surrounding block
-    let block = Block::default()
+    let sources_block = Block::default()
         .borders(Borders::ALL)
         .title("sources")
         .title_alignment(Alignment::Center)
         .border_type(BorderType::Rounded);
-    f.render_widget(block, chunks[0]);
+    let sources_block = if app.selected == 0 {
+        sources_block.border_style(Style::default().fg(Color::LightGreen))
+    } else {
+        sources_block
+    };
+    f.render_widget(sources_block, chunks[0]);
 
     let chunks = Layout::default()
     .direction(Direction::Vertical)
@@ -87,12 +245,73 @@ fn ui<B: Backend>(f: &mut Frame<B>) {
         .title("articles list")
         .title_alignment(Alignment::Center)
         .border_type(BorderType::Rounded);
+    let block = if app.selected == 1 {
+        block.border_style(Style::default().fg(Color::LightGreen))
+    } else {
+        block
+    };
     f.render_widget(block, chunks[0]);
+    let bar = chunks[0];
 
     let block = Block::default()
         .borders(Borders::ALL)
         .title("article")
         .title_alignment(Alignment::Center)
         .border_type(BorderType::Rounded);
+    let block = if app.selected == 2 {
+        block.border_style(Style::default().fg(Color::LightGreen))
+    } else {
+        block
+    };
     f.render_widget(block, chunks[1]);
+
+    // Iterate through all elements in the `items` app and append some debug text to it.
+    let items: Vec<ListItem> = app
+        .items
+        .items
+        .iter()
+        .map(|i| {
+            let mut lines = vec![Spans::from(i.0)];
+            ListItem::new(lines) //.style(Style::default().fg(Color::Black).bg(Color::White))
+        })
+        .collect();
+
+    // Create a List from all list items and highlight the currently selected one
+    let items = List::new(items)
+        .block(Block::default().borders(Borders::ALL).title("Sources"))
+        .highlight_style(
+            Style::default()
+                .bg(Color::LightGreen)
+                .fg(Color::Black)
+                .add_modifier(Modifier::BOLD),
+        )
+        .highlight_symbol("");
+
+    // We can now render the item list
+    f.render_stateful_widget(items, foo, &mut app.items.state);
+
+    // Iterate through all elements in the `items` app and append some debug text to it.
+    let articles: Vec<ListItem> = app
+        .articles
+        .items
+        .iter()
+        .map(|i| {
+            let mut lines = vec![Spans::from(i.0)];
+            ListItem::new(lines)
+        })
+        .collect();
+
+    // Create a List from all list items and highlight the currently selected one
+    let articles = List::new(articles)
+        .block(Block::default().borders(Borders::ALL).title("Articles"))
+        .highlight_style(
+            Style::default()
+                .bg(Color::LightGreen)
+                .fg(Color::Black)
+                .add_modifier(Modifier::BOLD),
+        )
+        .highlight_symbol("");
+
+    // We can now render the item list
+    f.render_stateful_widget(articles, bar, &mut app.articles.state);
 }
