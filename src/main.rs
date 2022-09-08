b549b58a2630e8bc0ee6cb03603bd027f94b3d24
Slavcho Slavchev <slavsan.mail@gmail.com>
2022-09-09 00:05:51 +0300

    Add menu in footer

--- src/main.rs
+++ src/main.rs
@@ -238,12 +238,32 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
     let size = f.size();
 
     let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Min(20),
+            Constraint::Length(1),
+        ].as_ref())
+        .split(f.size());
+
+    let block = Block::default()
+        .borders(Borders::NONE)
+        .title_alignment(Alignment::Center)
+        .border_type(BorderType::Rounded);
+    let text = vec![
+        Spans::from(vec![
+            Span::styled(":: quit[q] | next[tab] | down/up[j/k] ::", Style::default().fg(Color::Blue)),
+        ]),
+    ];
+    let paragraph = Paragraph::new(text).block(block).wrap(Wrap { trim: true });
+    f.render_widget(paragraph, chunks[1]);
+
+    let chunks = Layout::default()
         .direction(Direction::Horizontal)
         .constraints([
             Constraint::Percentage(30),
             Constraint::Percentage(70),
         ].as_ref())
-        .split(f.size());
+        .split(chunks[0]);
 
     let foo = chunks[0];
 
